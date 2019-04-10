93f8413f698f5a575e54fadc275505c4e787b631
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-10 12:00:58 -0400

    Start to load data from database

--- asoiaf/urls.py
+++ asoiaf/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 from . import views
-# from rest_framework.routers import DefaultRouter
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('books', views.AllBooks.as_view(), name="all-books"),
-    path('books/<int:pk>', views.BookDetail.as_view(), name='book-detail'),
-    path('characters', views.AllCharacters.as_view(), name='all-characters'),
-    path('characters/<int:pk>', views.CharacterDetail.as_view(),
-         name='character-detail')
+    ,
 ]
--- asoiaf/views.py
+++ asoiaf/views.py
@@ -1,32 +1,11 @@
-from rest_framework import generics
-from .serializers import BookSerializer, CharacterSerializer
-from .models import Book, Character
+from .models import Book, Character, Nobility, Death, Book_of_Death, Chapter_of_Death
 from django.shortcuts import render
-import requests
-
-api_url_base = 'http://localhost:8000'
+from random import randint
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
 
 
 def homepage(request):
-    response = requests.get(api_url_base+"/characters").json()
-    return render(request, 'asoiaf/homepage.html', {'characters': response})
-
-
-class AllBooks(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class AllCharacters(generics.ListCreateAPIView):
-    queryset = Character.objects.all()
-    serializer_class = CharacterSerializer
-
-
-class CharacterDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Character.objects.all()
-    serializer_class = CharacterSerializer
+    female_chars = len(Character.objects.filter(gender="Female"))
+    male_chars = len(Character.objects.filter(gender="Male"))
+    return render(request, 'asoiaf/homepage.html', {'female_chars': female_chars, 'male_chars': male_chars})
