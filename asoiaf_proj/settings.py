fa6803400d24075d9db8f3bba26cffcee7afe746
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-10 19:52:03 -0400

    Reset default database

--- asoiaf_proj/settings.py
+++ asoiaf_proj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,14 +82,16 @@ WSGI_APPLICATION = 'asoiaf_proj.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'asoiaf',
-        'USER': 'asoiafuser',
-        'PASSWORD': 'asoiafadmin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+    # 'ENGINE': 'django.db.backends.postgresql',
+    # 'NAME': 'asoiaf',
+    # 'USER': 'asoiafuser',
+    # 'PASSWORD': 'asoiafadmin',
+    # 'HOST': 'localhost',
+    # 'PORT': '5432',
+
 }
 
 
