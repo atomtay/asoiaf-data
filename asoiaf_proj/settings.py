e106d2b1374ca017d9829273b9f8262cd2c3143e
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-10 20:38:39 -0400

    add dj database url

--- asoiaf_proj/settings.py
+++ asoiaf_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 # from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,6 +81,12 @@ WSGI_APPLICATION = 'asoiaf_proj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # DATABASES = {
 #     # 'default': dj_database_url.config(
