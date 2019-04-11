4b6de595ea63c8d281984edbf8d26197bd6868af
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-11 14:00:41 -0400

    Collecting chapter numbers

--- asoiaf/urls.py
+++ asoiaf/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import HomeView
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', HomeView.as_view()),
-    path('line_chart/', views.line_chart,
-         name='line_chart'),
+
+    path('line_chart', views.line_chart),
     url(r'^line_chart/json/$', views.line_chart_json,
         name='line_chart_json'),
+    url(r'^new_chart/$', views.new_chart,
+        name='new_chart'),
+    url(r'^new_chart/json/$', views.new_chart_json,
+        name='new_chart_json'),
 
 ]
--- asoiaf/views.py
+++ asoiaf/views.py
@@ -8,49 +8,17 @@ from chartjs.views.lines import BaseLineChartView
 from django.db import connection
 
 
-class HomeView(TemplateView):
-    def my_custom_sql(self):
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT name from asoiaf_character INNER JOIN asoiaf_death ON asoiaf_character.name = asoiaf_death.name_id WHERE gender = 'Male'")
-            row = len(cursor.fetchall())
-
-        return row
-
-    dead = Death.objects.all()
-    male = Character.objects.filter(gender='Male')
-    # dead_male_chars = len  Character.objects.select_related('asoiaf_nobility.name'))
-
-    #len(Character.objects.filter(   death__name='Robert Baratheon'))
-
-    def get(self, request):
-        return render(request, "homepage.html", {'book': self.my_custom_sql})
-
-
 class LineChartJSONView(BaseLineChartView):
     def get_labels(self):
         return ["Total", "Alive", "Dead"]
 
     def get_providers(self):
-        """Return names of datasets."""
         return ["Total", "Male", "Female"]
 
-    def my_custom_sql(self):
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT name from asoiaf_character INNER JOIN asoiaf_death ON asoiaf_character.name = asoiaf_death.name_id WHERE gender = 'Male'")
-            row = len(cursor.fetchall())
-
-        return row
-
     def get_data(self):
-        """Return 3 datasets to plot."""
-
         chars_total = len(Character.objects.all())
         male_chars_total = len(Character.objects.filter(gender='Male'))
         female_chars_total = len(Character.objects.filter(gender='Female'))
-
-        dead_chars_total = len(Death.objects.all())
 
         with connection.cursor() as cursor:
             cursor.execute(
@@ -60,30 +28,42 @@ class LineChartJSONView(BaseLineChartView):
             cursor.execute(
                 "SELECT name from asoiaf_character INNER JOIN asoiaf_death ON asoiaf_character.name = asoiaf_death.name_id WHERE gender = 'Female'")
             femalerow = len(cursor.fetchall())
+
+        dead_chars_total = len(Death.objects.all())
         dead_male_chars = malerow
         dead_female_chars = femalerow
-
-        # SELECT count(name) from asoiaf_character INNER JOIN asoiaf_death ON asoiaf_character.name = asoiaf_death.name_id WHERE "gender" = 'Male'
-
-        # dead_male_chars = 0
-        # dead_male_chars = len(Death.objects.filter(character__gender='Male'))
-        # dead_male_chars = len(Death.objects.select_related('name'))
-
-        # Person.objects.raw(
-        #     'SELECT count(name) from asoiaf_character INNER JOIN asoiaf_death ON asoiaf_character.name=asoiaf_death.name_id WHERE "gender"=\'Male\'')
 
         alive_chars_total = chars_total - dead_chars_total
         alive_male_chars = male_chars_total - dead_male_chars
         alive_female_chars = female_chars_total - dead_female_chars
 
-        # all characters
         return [[chars_total, alive_chars_total, dead_chars_total],
                 [male_chars_total, alive_male_chars, dead_male_chars],
                 [female_chars_total, alive_female_chars, dead_female_chars]]
-        # male characters only
-        # [len(male_chars_total), len(alive_male_chars), 0],
-        # [len(female_chars_total), len(alive_female_chars), len(dead_female_chars)]]  # female characters only
 
 
 line_chart = TemplateView.as_view(template_name='line_chart.html')
 line_chart_json = LineChartJSONView.as_view()
+
+
+class NewChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        return ["A Game of Thrones", "A Clash of Kings", "A Storm of Swords", "A Feast for Crows", "A Dance with Dragons"]
+
+    def get_providers(self):
+        return ["Male", "Female", "Total Chapter"]
+
+    def get_data(self):
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT num_of_chapters FROM asoiaf_book;")
+            chapter_list = []
+            for chapter in cursor.fetchall():
+                chapter_list.append(chapter[0])
+        return [[75, 44, 92, 11, 44],
+                [41, 92, 18, 3, 73],
+                chapter_list]
+
+
+new_chart = TemplateView.as_view(template_name='new_chart.html')
+new_chart_json = NewChartJSONView.as_view()
